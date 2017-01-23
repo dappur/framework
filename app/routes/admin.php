324299cb1a442107e4ef8bb238e7febf5a0b2a4e
Jeff Thomas <jeffmthomas@gmail.com>
2017-01-23 08:55:38 -0800

    prepare for dev release

--- app/routes/admin.php
+++ app/routes/admin.php
@@ -1,5 +1,35 @@
 <?php
 
-$app->group('/dashboard', function () {
-    $this->get('', 'AdminController:dashboard')->setName('dashboard');
-})->add(new App\Middleware\AdminMiddleware($container))->add(new App\Middleware\AuthMiddleware($container));
+$app->group('/dashboard', function () use($app) {
+	// Dashboard Home
+    $app->get('', 'AdminController:dashboard')
+    	->setName('dashboard');
+
+    // Users Routes
+    $app->group('/users', function() use ($app) {
+
+        // User List
+        $app->get('', 'AdminController:users')
+            ->setName('admin-users');
+
+        // Add New User
+        $app->map(['GET', 'POST'], '/add', 'AdminController:usersAdd')
+            ->setName('admin-users-add');
+
+        // Edit User
+        $app->map(['GET', 'POST'], '/edit[/{username}]', 'AdminController:usersEdit')
+            ->setName('admin-users-edit');
+
+    });
+
+    // Account Settings
+    $app->get('/settings', 'AdminController:settings')
+    	->setName('admin-settings');
+
+    // Global Settings
+    $app->get('/global-settings', 'AdminController:settingsGlobal')
+    	->setName('settings-global');
+})
+->add(new App\Middleware\AdminMiddleware($container))
+->add(new App\Middleware\AuthMiddleware($container));
+
--- app/src/Controller/AdminController.php
+++ app/src/Controller/AdminController.php
@@ -9,7 +9,44 @@ class AdminController extends Controller{
 
     public function dashboard(Request $request, Response $response){
 
-        return $this->view->render($response, 'App/home.twig');
+        return $this->view->render($response, 'Admin/dashboard.twig');
+
+    }
+
+    public function users(Request $request, Response $response){
+
+
+        $users = new \App\Model\Users;
+
+        return $this->view->render($response, 'Admin/users.twig', ["users" => $users->get()]);
+
+    }
+
+    public function usersEdit(Request $request, Response $response, $username){
+        
+        $users = new \App\Model\Users;
+        $user = $users->where('username', '=', $username)->first();
+
+        $roles = new \App\Model\Roles;
+
+        if ($user) {
+            return $this->view->render($response, 'Admin/users-edit.twig', ['user' => $user, 'roles' => $roles]);
+        }else{
+            $this->flash('danger', 'Sorry, that user was not found.');
+            return $response->withRedirect($this->router->pathFor('admin-users'));
+        }
+        
+    }
+
+    public function settings(Request $request, Response $response){
+
+        return $this->view->render($response, 'Admin/settings.twig');
+
+    }
+
+    public function settingsGlobal(Request $request, Response $response){
+
+        return $this->view->render($response, 'Admin/global-settings.twig');
 
     }
 
