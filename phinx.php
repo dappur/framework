5a5c1616acf1d9772105d46c57c58e7d2905a59c
Jeff Thomas <jeffmthomas@gmail.com>
2017-03-24 09:25:37 -0700

    added custom phinx template

--- phinx.php
+++ phinx.php
@@ -5,20 +5,23 @@ $dbconf = $settings['settings']['db'];
 
 
 return [
-  'paths' => [
-    'migrations' => 'database/migrations'
-  ],
-  'migration_base_class' => '\App\Migration\Migration',
-  'environments' => [
+  	'paths' => [
+    	'migrations' => 'database/migrations'
+  	],
+  	'templates' => [
+        'file' => '%%PHINX_CONFIG_DIR%%/database/templates/create-template.php'
+  	],
+  	'migration_base_class' => '\App\Migration\Migration',
+  	'environments' => [
     'default_migration_table' => 'phinxlog',
     'default_database' => 'dev',
     'dev' => [
-      'adapter' => $dbconf['driver'],
-      'host' => $dbconf['host'],
-      'name' => $dbconf['database'],
-      'user' => $dbconf['username'],
-      'pass' => $dbconf['password'],
-      'port' => $dbconf['port']
+      	'adapter' => $dbconf['driver'],
+      	'host' => $dbconf['host'],
+      	'name' => $dbconf['database'],
+      	'user' => $dbconf['username'],
+      	'pass' => $dbconf['password'],
+      	'port' => $dbconf['port']
     ]
   ]
 ];
