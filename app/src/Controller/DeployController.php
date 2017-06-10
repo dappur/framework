eabab2c717d4cd5fc40b20c9872330bcac4f2a19
Jeff Thomas <jeffmthomas@gmail.com>
2017-06-09 18:24:15 -0700

    added logging for deployment

--- app/src/Controller/DeployController.php
+++ app/src/Controller/DeployController.php
@@ -16,12 +16,14 @@ class DeployController extends Controller
 	{
 
 		if (!$this->settings['deployment']['enabled']) {
-			$this->flash->addMessage('warning', 'Deployment is not enabled in the config.');
+			$this->flash->addMessage('danger', 'Deployment is not enabled in the config.');
+			$this->logger->addError("Deployment", array("message" => "Deployment is not enabled in the config."));
             return $response->withRedirect($this->router->pathFor('home'));
 		}
 
 		if ($this->settings['deployment']['repo_url'] == "") {
 			$this->flash->addMessage('danger', 'Please specify a repo url in your deployment config.');
+			$this->logger->addError("Deployment", array("message" => "Please specify a repo url in your deployment config."));
             return $response->withRedirect($this->router->pathFor('home'));
 		}
 
