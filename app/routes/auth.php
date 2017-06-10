64e65c51aed9005675bd78c1a2058a81668cbd0b
Jeff Thomas <jeffmthomas@gmail.com>
2017-06-09 18:59:06 -0700

    added csrf

--- app/routes/auth.php
+++ app/routes/auth.php
@@ -1,11 +1,14 @@
 <?php
 
-$app->group('', function () {
-    $this->map(['GET', 'POST'], '/login', 'AuthController:login')->setName('login');
-    $this->map(['GET', 'POST'], '/register', 'AuthController:register')->setName('register');
-    $this->map(['GET', 'POST'], '/forgot-password', 'AuthController:forgotPassword')->setName('forgot-password');
-})->add(new Dappur\Middleware\GuestMiddleware($container));
+$app->group('/', function () {
+    $this->map(['GET', 'POST'], 'login', 'AuthController:login')->setName('login');
+    $this->map(['GET', 'POST'], 'register', 'AuthController:register')->setName('register');
+    $this->map(['GET', 'POST'], 'forgot-password', 'AuthController:forgotPassword')->setName('forgot-password');
+})
+->add(new Dappur\Middleware\GuestMiddleware($container))
+->add(new \Dappur\Middleware\CsrfMiddleware($container))
+->add($container->get('csrf'));
 
 $app->group('', function () {
     $this->get('/logout', 'AuthController:logout')->setName('logout');
-})->add(new Dappur\Middleware\AuthMiddleware($container));
+});
