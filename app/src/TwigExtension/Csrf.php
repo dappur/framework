bae5bae570bb7238c4ce9e6e9c5589593f8399b2
Jeff Thomas <jeffmthomas@gmail.com>
2018-04-08 14:04:41 -0700

    phpmd fix

--- app/src/TwigExtension/Csrf.php
+++ app/src/TwigExtension/Csrf.php
@@ -51,7 +51,9 @@ class Csrf extends \Twig_Extension
 
     public function csrf()
     {
-        return '<input type="hidden" name="' . $this->csrf->getTokenNameKey() . '" value="' . $this->csrf->getTokenName() . '"><input type="hidden" name="' . $this->csrf->getTokenValueKey() .
+        return '<input type="hidden" name="' . $this->csrf->getTokenNameKey() .
+            '" value="' . $this->csrf->getTokenName() .
+            '"><input type="hidden" name="' . $this->csrf->getTokenValueKey() .
             '" value="' . $this->csrf->getTokenValue() . '">';
     }
 }
