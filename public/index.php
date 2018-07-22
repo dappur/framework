2684ec80bea9132b57a18f148a90cff8b2386e4d
Edward Teach <edward@edwardteach.co>
2018-07-21 23:20:37 -0700

    phpcs fixes

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-session_set_cookie_params(null,null,null,null,true); 
+session_set_cookie_params(null, null, null, null, true);
 session_start();
 
 // Load Vendor Autoload
@@ -7,7 +7,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // Load Settings
 $settings_file = file_get_contents(__DIR__ . '/../settings.json');
-$settings = json_decode($settings_file, TRUE);
+$settings = json_decode($settings_file, true);
 
 $app = new Slim\App(array('settings' => $settings));
 
@@ -21,8 +21,7 @@ date_default_timezone_set($container['config']['timezone']);
 require __DIR__ . '/../app/bootstrap/controllers.php';
 
 // Load Routes
-foreach (glob(__DIR__ . '/../app/routes/*.php') as $filename)
-{
+foreach (glob(__DIR__ . '/../app/routes/*.php') as $filename) {
     require $filename;
 }
 
