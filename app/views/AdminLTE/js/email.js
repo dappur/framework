0df7bac28a1825edf5ed1c2f50b39f108fdffee1
Edward Teach <edward@edwardteach.co>
2018-07-28 12:12:16 -0700

    cleaned up dashboard JS

--- app/views/AdminLTE/js/email.js
+++ app/views/AdminLTE/js/email.js
@@ -172,7 +172,6 @@ $(function () {
         });
     });
 
-
     function validateEmail(email) {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
