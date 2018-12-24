1789661ea0467c259e3cb757a0d702eb75cd5be1
Edward Teach <edward@edwardteach.co>
2018-12-24 14:51:17 -0800

    test

--- app/src/Middleware/Admin.php
+++ app/src/Middleware/Admin.php
@@ -6,11 +6,11 @@ class Admin extends Middleware
 {
     public function __invoke($request, $response, $next)
     {
-        if (!$this->auth->hasAccess('dashboard.*')) {
-            $this->flash->addMessage('danger', 'You do not have sufficient privileges to access this page!');
-            return $response->withRedirect($this->router->pathFor('home'));
+        if ($this->auth->check() && $this->auth->hasAccess('dashboard.*')) {
+        	return $next($request, $response);
         }
 
-        return $next($request, $response);
+        $this->flash->addMessage('danger', 'You do not have sufficient privileges to access this page!');
+        return $response->withRedirect($this->router->pathFor('home'));
     }
 }
