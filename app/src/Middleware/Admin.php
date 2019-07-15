59fd1f2dbbc474aa493f7a6649dba19ea8cdbfb9
Jeff Thomas <jeffmthomas@gmail.com>
2019-07-14 23:51:52 -0700

    updated themes

--- app/src/Middleware/Admin.php
+++ app/src/Middleware/Admin.php
@@ -7,7 +7,7 @@ class Admin extends Middleware
     public function __invoke($request, $response, $next)
     {
         if ($this->auth->check() && $this->auth->hasAccess('dashboard.*')) {
-        	return $next($request, $response);
+            return $next($request, $response);
         }
 
         $this->flash->addMessage('danger', 'You do not have sufficient privileges to access this page!');
