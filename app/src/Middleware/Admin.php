7f59e6bae35a863b4f06eb64388cb623b9810bf4
Jeff Thomas <jeffmthomas@gmail.com>
2019-07-15 00:03:40 -0700

    updated format

--- app/src/Middleware/Admin.php
+++ app/src/Middleware/Admin.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Dappur\Middleware;
-
 class Admin extends Middleware
 {
     public function __invoke($request, $response, $next)
@@ -13,7 +11,6 @@ class Admin extends Middleware
         if (!$this->auth->hasAccess('dashboard.*')) {
             return $response->withRedirect($this->router->pathFor('home'));
         }
-
         return $next($request, $response);
     }
 }
